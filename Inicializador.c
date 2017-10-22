c0918f857acb6d487e660221fa3aaa46f86fedee
luisfqv2194 <luisfqv94@gmail.com>
2017-10-22 10:33:01 -0600

    Ya se crea la memoria compartida
    Creo memoria compartida de numeros  y se pueden mostrar en pantalla

--- Inicializador.c
+++ Inicializador.c
@@ -1,12 +1,14 @@
-
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <semaphore.h>
 
-
-
-
+//IMPORTANT; To delete shared memory use ipcrm -M <key>
 
 
 int main(int argc , char *argv[]){
@@ -20,14 +22,45 @@ int main(int argc , char *argv[]){
     key_t key;
     int *shm, *s, *m;
 	sem_t *sem;
+	sem_t *semb;
 
 	key = 6969;
 
-	puts("Ingrese la cantidad de paginas con las que desea trabajar: ");
+	puts("Ingrese la cantidad de enteros a guardar: ");
     	scanf("%d",&cantLineas);
 
  //Create the segment
 
-   
+    if ((shmid = shmget(key, sizeof(int)*cantLineas, IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        return(1);
+	}
+
+	if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
+        perror("shmat");
+        return(1);
+    }
+
+     /* Now put some things into the memory for the
+     * other process to read.
+     */
+
+    s = shm;
+
+    for (c = 0; c <= cantLineas; c++){
+        *s++ = -1;
+	}
+    s = NULL;
+	m=shm;
+
+
+	for(verificador = 0;verificador <=cantLineas; verificador++){
+		printf("%d ",*m);
+		m++;
+	}
+	m = NULL;
+
+	return 1;
+	
 }
 
